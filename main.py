9ab52ce6d7578a147a41e6bd7bc9fb3090cc20de
JasonWuuu <wucong60@gmail.com>
2019-12-18 17:22:38 +0800

    'init'

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv, find_dotenv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def show_hello():
+    myname = os.getenv('myname')
+
+    return myname
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    app.run(port=8000)
