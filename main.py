a72f4a710007ffdfcf33ff1b845fafda083cafe7
cong.wu <cong.wu@accenture.com>
2019-12-24 16:56:21 +0800

    加了部署到GCP app engine 的配置文件

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def show_hello():
     myname = os.getenv('myname')
-
     return myname
 
 
 if __name__ == '__main__':
     load_dotenv()
-    app.run(port=8000)
+    app.run(port=8080)
